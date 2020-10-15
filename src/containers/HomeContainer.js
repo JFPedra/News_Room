14fd84975f5f594fa057fa1209f05ef1dddc2f5e
JFPedra <63618507+JFPedra@users.noreply.github.com>
2020-10-15 17:15:14 -0500

    mapStatetoProps bug fixed

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,6 +1,6 @@
 import Home from "../components/Home";
 import { connect } from "react-redux";
 
-const mapStatetoProps = (state) => state;
+const mapStatetoProps = (state) => ({ state });
 
 export default connect(mapStatetoProps)(Home);
